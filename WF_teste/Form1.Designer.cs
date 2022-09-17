fa37d03e434cc41130d28847f3855f9749352cc4
rodolfo.gsilva2 <rodolfo.gsilva2@senacsp.edu.br>
2022-09-16 21:22:28 -0300

    Adicionar arquivos de projeto.

---
+++ WF_teste/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace WF_teste
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.btnExibir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(135, 156);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(182, 20);
+            this.txtNome.TabIndex = 0;
+            this.txtNome.Text = "Nome completo";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(135, 104);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(100, 13);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "Informe o seu nome";
+            // 
+            // btnExibir
+            // 
+            this.btnExibir.Location = new System.Drawing.Point(138, 258);
+            this.btnExibir.Name = "btnExibir";
+            this.btnExibir.Size = new System.Drawing.Size(75, 23);
+            this.btnExibir.TabIndex = 2;
+            this.btnExibir.Text = "Clique Aqui";
+            this.btnExibir.UseVisualStyleBackColor = true;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExibir);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.txtNome);
+            this.Name = "frmPrincipal";
+            this.Text = "Meu primeiro Programa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Button btnExibir;
+    }
+}
+
